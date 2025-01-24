43214fcec5f46d5ee7eef62f4c4327852ab7b4af
Divyansh Tripathi <tripathidivy5647@gmail.com>
2025-01-24 23:51:17 +0530

    added user specific logic

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -2,60 +2,49 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import LogoutButton from "./LogoutButton";
-
-// async function fetchUserSpecificData(username: string) {
-//   const res = await fetch(`/api/user/data?username=${username}`, {
-//     credentials: 'include', 
-
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   });
-
-//   console.log('Fetch Response:', {
-//     status: res.status,
-//     ok: res.ok,
-//     headers: Object.fromEntries(res.headers.entries())
-//   });
-
-//   const responseText = await res.text();
-//   console.log('Response Text:', responseText);
-
-//   if (res.ok) {
-//     return JSON.parse(responseText);
-//   }
-
-//   throw new Error(`API Error: ${res.status} - ${responseText}`);
-// }
+import { prisma } from "@repo/db"
 
 export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
 
   if (!session?.user) {
-    redirect("/login");
+    redirect("/signin");
   }
 
-  // let userData;
-  // try {
-  //   userData = await fetchUserSpecificData(session.user.username);
-  // } catch (error) {
-  //   userData = { message: "Error loading user data: ", error };
-  // }
+  const id = Number(session.user.id);
+  const username = session.user.username;
+
+  const comments = await fetchComments(id);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">
-        Welcome, {session.user.username || "User"}!
+        Welcome, {username || "User"}!
       </h1>
 
-      {/* <div className="bg-gray-100 p-4 rounded">
-        <h2 className="text-xl mb-2">Your Data:</h2>
-        <pre className="bg-white p-2 rounded overflow-x-auto">
-          {JSON.stringify(userData, null, 2)}
-        </pre>
-      </div> */}
+      <div>
+        <h2>{session.user.username}&aposs comments: </h2>
+        <ul>
+          {comments.length > 0 ? (comments.map((comment) =>
+            <li key={comment.id}>
+              {comment.content}
+            </li>
+          )) : (
+            <p>no comments done</p>
+          )}
+        </ul>
+      </div>
 
       <LogoutButton />
     </div>
   );
+}
+
+async function fetchComments(id: number) {
+  const comments = await prisma.comment.findMany({
+    where: { userId: id }
+  })
+
+  return comments;
+
 }
